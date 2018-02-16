c4d295c35e6fb4b67e7e7ce72e9d10099f4ea39e
Tyler Wilcox <tywilly300@gmail.com>
2018-02-16 17:16:59 -0500

    Basic console I/O

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
 
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main(int c, char *args[]){
 
    cout << "Hello World!" << endl;
+
+   cout << "Enter your name: ";
+
+   string name;
+
+   getline(cin, name);
+
+   cout << "Welcome " << name << "!" << endl;
+
+
+   return 0;
 
 }
 
